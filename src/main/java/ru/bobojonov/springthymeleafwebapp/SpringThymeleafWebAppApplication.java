2a3f31a13ebb57a3ce5763544b16a53e704a0458
vivar <bobojonoffff@gmail.com>
2023-12-15 21:50:21 +0300

    init commit

---
+++ src/main/java/ru/bobojonov/springthymeleafwebapp/SpringThymeleafWebAppApplication.java
@@ -0,0 +1,13 @@
+package ru.bobojonov.springthymeleafwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymeleafWebAppApplication.class, args);
+	}
+
+}
